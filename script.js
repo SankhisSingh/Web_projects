5ba0f1c158661ddd3a70f1d17a398d45da62b1a8
SankhiSingh <singhssankhi@gmail.com>
2025-06-11 19:22:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 alert("welcome to sigma web development course")
+console.log("Hey my name is sankhi")
+var a = prompt("Enter a number")
+var isTrue=confirm("it will directly run")
+if(isTrue){
+  console.log("I am ok")
+}
+else{
+  console.log("I am not ok")
+}
+console.log("The number is "+a)
+document.title="This is title of website"
+document.body.style.backgroundColor="red"
