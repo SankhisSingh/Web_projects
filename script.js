c8479f99315c1ba51d83c01314abfd42abca46a4
SankhiSingh <singhssankhi@gmail.com>
2025-06-12 18:31:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ else{
 console.log("The number is "+a)
 document.title="This is title of website"
 document.body.style.backgroundColor="red"
+//This is about data types of js
+var a = 3;
+var b= 4;
+console.log("sum of a and b is", a+b)
+var c= "sankhi";
+console.log("Sum is " a+b+c)
+//output will be 7sankhi
+console.log(typeof a, typeof b, typeof c)
+//we use typeof to check the datatype of our variable in js
