32093a172f4c8bda6470d92c852d10ab70c8a17a
SankhiSingh <singhssankhi@gmail.com>
2025-06-12 18:33:35 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,25 @@
+console.log("Hello world")
+console.log("are you sure ")
+alert("welcome to sigma web development course"
+
+)
+console.log("code is running...")
+console.log("Code is running like a wow")
+var a=prompt("Enter a number")
+var isTrue= confirm("are you sure")
+if(isTrue)
+{
+    console.log("I am ok")
+
+}
+else{
+    console.log("I am not ok")
+}
+console.log("Your number is " + a)
+document.title= "Hey I am good"
+document.body.style.backgroundColor="red"
+var c= 2;
+var d= 3;
+var e= "sankhi";
+console.log("sum of c and d is ", c + d + e)
+console.log(typeof c, typeof d, typeof e)
