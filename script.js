ed441f66e80590e1aea77de80501bc12571d042c
SankhiSingh <singhssankhi@gmail.com>
2025-06-16 19:30:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-alert("welcome to sigma web development course")
-console.log("Hey my name is sankhi")
-var a = prompt("Enter a number")
-var isTrue=confirm("it will directly run")
-if(isTrue){
-  console.log("I am ok")
-}
-else{
-  console.log("I am not ok")
-}
-console.log("The number is "+a)
-document.title="This is title of website"
-document.body.style.backgroundColor="red"
-//This is about data types of js
-var a = 3;
-var b= 4;
-console.log("sum of a and b is", a+b)
-var c= "sankhi";
-console.log("Sum is " a+b+c)
-//output will be 7sankhi
-console.log(typeof a, typeof b, typeof c)
-//we use typeof to check the datatype of our variable in js
+//alert("welcome to sigma web development course"//
+
+   // console.log("code is running...")
+//)
+//console.log("Code is running like a wow")
+//var a=prompt("Enter a number")
+//var isTrue= confirm("are you sure")
+//if(isTrue)
+//{
+//    console.log("I am ok")
+
+//}
+//else{
+   // console.log("I am not ok")
+//}
+//console.log("Your number is " + a)
+//document.title= "Hey I am good"
+//document.body.style.backgroundColor="red"
+//var c= 2;
+//var d= 3;
+//var e= "sankhi"
+//console.log("sum of c and d is ", c + d + e)
+//console.log(typeof c, typeof d, typeof e)
+//string
+console.log("this is string tutorial")
+let a = "Sankhi"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a.length)
+let n="sankhi"
+let friend="Suchi"
+console.log("Her name is " + n +  " Her friend name is "  + friend )
+console.log(`her name is ${n} and her friend's name is ${friend}`)
+let b= "Apple"
+console.log(b.toUpperCase())
+console.log(b.toLocaleLowerCase())
+console.log(b.split())
+console.log(b.trim(2))
+console.log(b.concat(a))
+console.log(b.lastIndexOf())
+console.log(b.substring())
+console.log(b.slice(1,4))
+console.log( b.replace("pp","22"))
+console.log(b.charAt(1))
